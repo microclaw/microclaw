c93915dca70a6b22d8b00f3c52c4ce22307bf39d
Everett <everettjf@live.com>
2026-08-15 13:10:34 -0700

    Add branded social preview

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -141,7 +141,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/social-card.svg',
+      image: 'img/social-card.png',
       colorMode: {
         defaultMode: 'dark',
         respectPrefersColorScheme: true,
